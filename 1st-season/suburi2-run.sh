072e8d6e96910092d26199b8d6f21c4e61ffc664
gamix255 <gamix255@gmail.com>
2016-08-09 21:47:29 +0900

    suburi2-run.sh not initials file fix.

--- 1st-season/suburi2-run.sh
+++ 1st-season/suburi2-run.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-# suburi2 main script 20160808
+# suburi2 entry script 20160808
 echo "## chekking && making work directory "
 
-echo '## making test data "echo 1234 > a.txt"'
+echo '## making test data "echo 1234 > ./work/a.txt"'
 
 if test ! -d ./work
   then mkdir ./work
 fi
 
-echo '## running suburi2.js ##'
+echo 1234 > ./work/a.txt
+
+echo '## running suburi2.js '
 
 ./suburi2.js
